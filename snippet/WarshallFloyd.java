87ddf9b3a6e3d791d33dccc59665f9d1aa9d7bb0
Ziphil <yfe23457@nifty.com>
2018-06-11 22:17:26 +0900

    ワーシャル・フロイド法で辺がない場合は最大整数値を指定させるよう変更

--- snippet/WarshallFloyd.java
+++ snippet/WarshallFloyd.java
@@ -6,14 +6,14 @@ public class Main implements Runnable {
 
   // ◆ 入力
   // ・n … 頂点数
-  // ・costs … 第 1 成分から第 2 成分への辺の重み (辺がない場合は 0)
+  // ・costs … 第 1 成分から第 2 成分への辺の重み (辺がない場合は Integer.MAX_VALUE)
   // ◆ 出力
   // ・dd … 最短距離
   public void warshallFloyd(int n, int[][] costs) {
     int[][] dd = new int[n][n];
     for (int i = 0 ; i < n ; i ++) {
       for (int j = 0 ; j < n ; j ++) {
-        dd[i][j] = (costs[i][j] > 0) ? costs[i][j] : Integer.MAX_VALUE;
+        dd[i][j] = costs[i][j];
       }
     }
     for (int k = 0 ; k < n ; k ++) {
