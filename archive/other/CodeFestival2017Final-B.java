3f5183f5cee4c6199c5852cb34f91bb96ed0d0b8
Ziphil <yfe23457@nifty.com>
2018-06-23 00:34:20 +0900

    提出コードを追加

---
+++ archive/other/CodeFestival2017Final-B.java
@@ -0,0 +1,137 @@
+import java.io.*;
+import java.util.*;
+ 
+ 
+public class Main implements Runnable {
+
+  @SuppressWarnings("unchecked") 
+  public void run() {
+    BetterScanner scanner = new BetterScanner(System.in);
+  
+    String s = scanner.next();
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    for (int i = 0 ; i < s.length() ; i ++) {
+      char x = s.charAt(i);
+      if (x == 'a') {
+        a ++;
+      } else if (x == 'b') {
+        b ++;
+      } else {
+        c ++;
+      }
+    }
+
+    boolean p = Math.abs(a - b) <= 1 && Math.abs(b - c) <= 1 && Math.abs(c - a) <= 1;
+    System.out.println((p) ? "YES" : "NO");
+  }
+
+  public static void main(String[] args) {
+    Main main = new Main();
+    main.run();
+  }
+ 
+  public static class BetterScanner {
+ 
+    private InputStream stream;
+    private byte[] buffer = new byte[1024];
+    private int pointer = 0;
+    private int bufferLength = 0;
+ 
+    public BetterScanner(InputStream stream) {
+      this.stream = stream;
+    }
+ 
+    private boolean updateBuffer() {
+      if (pointer >= bufferLength) {
+        pointer = 0;
+        try {
+          bufferLength = stream.read(buffer);
+        } catch (IOException exception) {
+          exception.printStackTrace();
+        }
+        return bufferLength > 0;
+      } else {
+        return true;
+      }
+    }
+ 
+    private int read() {
+      if (updateBuffer()) {
+        return buffer[pointer ++];
+      } else {
+        return -1;
+      }
+    }
+ 
+    public boolean hasNext() {
+      skipUnprintable();
+      return updateBuffer();
+    }
+ 
+    private void skipUnprintable() { 
+      while (updateBuffer() && !isPrintableChar(buffer[pointer])) {
+        pointer ++;
+      }
+    }
+ 
+    public String next() {
+      if (hasNext()) {
+        StringBuilder builder = new StringBuilder();
+        int codePoint = read();
+        while (isPrintableChar(codePoint)) {
+          builder.appendCodePoint(codePoint);
+          codePoint = read();
+        }
+        return builder.toString();
+      } else {
+        throw new NoSuchElementException();
+      }
+    }
+ 
+    public long nextLong() {
+      if (hasNext()) {
+        long number = 0;
+        boolean minus = false;
+        int codePoint = read();
+        if (codePoint == '-') {
+          minus = true;
+          codePoint = read();
+        }
+        if (codePoint >= '0' && codePoint <= '9') {
+          while (true) {
+            if (codePoint >= '0' && codePoint <= '9') {
+              number *= 10;
+              number += codePoint - '0';
+            } else if (codePoint < 0 || !isPrintableChar(codePoint)) {
+              return (minus) ? -number : number;
+            } else {
+              throw new NumberFormatException();
+            }
+            codePoint = read();
+          }
+        } else {
+          throw new NumberFormatException();
+        }
+      } else {
+        throw new NoSuchElementException();
+      }
+    }
+ 
+    public int nextInt() {
+      long number = nextLong();
+      if (number >= Integer.MIN_VALUE && number <= Integer.MAX_VALUE) {
+        return (int)number;
+      } else {
+        throw new NumberFormatException();
+      }
+    }
+ 
+    private boolean isPrintableChar(int codePoint) {
+      return codePoint >= 33 && codePoint <= 126;
+    }
+ 
+  }
+ 
+}
