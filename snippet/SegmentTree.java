32ae0c598480db4736f349435357c3b33564ca3d
Ziphil <yfe23457@nifty.com>
2018-07-08 00:09:40 +0900

    セグメント木のスニペットをリファクタリング

--- snippet/SegmentTree.java
+++ snippet/SegmentTree.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.util.function.*;
  
  
 public class Main implements Runnable {
@@ -8,47 +7,54 @@ public class Main implements Runnable {
   public static class SegmentTree {
 
     private int size = 1;
-    private int[] array;
-    private int first;
-    private IntBinaryOperator operator;
+    private long[] array;
+    private long unit;
+    private Operator operator;
 
-    public SegmentTree(int n, int first, IntBinaryOperator operator) {
-      this.first = first;
+    public SegmentTree(int n, long unit, Operator operator) {
+      this.unit = unit;
       this.operator = operator;
       while (size < n) {
         size *= 2;
       }
-      array = new int[2 * size - 1];
-      Arrays.fill(array, first);
+      array = new long[2 * size - 1];
+      Arrays.fill(array, unit);
     }
 
-    public void update(int i, int data) {
+    public long get(int i) {
+      return array[i + size - 1];
+    }
+
+    public void set(int i, long data) {
       i += size - 1;
       array[i] = data;
       while (i > 0) {
         i = (i - 1) / 2;
-        array[i] = operator.applyAsInt(array[i * 2 + 1], array[i * 2 + 2]);
+        array[i] = operator.operate(array[i * 2 + 1], array[i * 2 + 2]);
       }
     }
 
-    public int find(int a, int b) {
+    public long find(int a, int b) {
       return find(a, b, 0, 0, size);
     }
 
-    public int find(int a, int b, int i, int l, int r) {
+    public long find(int a, int b, int i, int l, int r) {
       if (a >= r || b <= l) {
-        return first;
+        return unit;
       } else if (a <= l && b >= r) {
         return array[i];
       } else {
         int m = (l + r) / 2;
-        return operator.applyAsInt(find(a, b, i * 2 + 1, l, m), find(a, b, i * 2 + 2, m, r));
+        return operator.operate(find(a, b, i * 2 + 1, l, m), find(a, b, i * 2 + 2, m, r));
       }
     }
 
-    public int get(int i) {
-      return array[i + size - 1];
-    }
+  }
+
+  @FunctionalInterface
+  public static interface Operator {
+
+    public long operate(long left, long right);
 
   }
 
